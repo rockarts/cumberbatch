4685a051d2fb6b96d1bf6d532728c2c3abb461b7
Steven Rockarts <steven@airtrail.co>
2024-07-22 21:22:48 -0600

    Initial Commit

---
+++ cumberbatch/cumberbatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  cumberbatchApp.swift
+//  cumberbatch
+//
+//  Created by Steven Rockarts on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct cumberbatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
